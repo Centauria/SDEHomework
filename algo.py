91b373e487ed3afe77a742efb78d1554a916f188
Centauria <centauriavc@gmail.com>
2020-01-10 14:49:46 +0800

    add spearman in algo

--- algo.py
+++ algo.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def pearson(x: pd.Series, y: pd.Series):
+    assert len(x) == len(y)
     m_x, m_y = x.mean(), y.mean()
     x_, y_ = x - m_x, y - m_y
     xy = x_ * y_
@@ -11,3 +12,20 @@ def pearson(x: pd.Series, y: pd.Series):
     yy = y_ ** 2
     r = xy.sum() / np.sqrt(xx.sum() * yy.sum())
     return r
+
+
+def spearman(x: pd.Series, y: pd.Series):
+    assert len(x) == len(y)
+    L = len(x)
+
+    def ranks(z: pd.Series) -> pd.Series:
+        s = z.sort_values(ascending=False)
+        zs = pd.DataFrame(s)
+        zs['r'] = [i + 1 for i in range(len(z))]
+        zs = zs.sort_index()
+        return zs['r']
+
+    d2 = (ranks(x) - ranks(y)) ** 2
+    r = 3 * d2.sum() / L / (L * L - 1)
+    rs = 1 - 2 * r
+    return rs
